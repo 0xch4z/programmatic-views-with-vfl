9c450bf1ee14ce5f108e2ba6c2d1cd8faf4bbdda
Charles Kenney <Charlesc.kenney@gmail.com>
2017-09-06 00:30:49 -0400

    Initial Commit

---
+++ programmatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  programmatic
+//
+//  Created by Charles Kenney on 9/6/17.
+//  Copyright © 2017 Charles Kenney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
